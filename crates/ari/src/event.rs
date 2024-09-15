9af2b3fc602111d8e5dacca332a0b243fc3665c2
gibbz00 <gabrielhansson00@gmail.com>
2024-09-15 16:43:31 +0200

    feat(ari): add `ChannelToneDetected`

--- crates/ari/src/event.rs
+++ crates/ari/src/event.rs
@@ -14,6 +14,7 @@ pub enum AsteriskEvent {
     ChannelDestroyed(Event<ChannelDestroyed>),
     ChannelHold(Event<ChannelHold>),
     ChannelUnhold(Event<ChannelUnhold>),
+    ChannelToneDetected(Event<ChannelToneDetected>),
     ChannelVarset(Event<ChannelVarset>),
     ChannelHangupRequest(Event<ChannelHangupRequest>),
     ChannelDialplan(Event<ChannelDialplan>),
@@ -79,6 +80,12 @@ pub struct ChannelHold {
 #[derive(Debug, Deserialize, Getters)]
 #[serde(rename_all = "snake_case")]
 pub struct ChannelUnhold {
+    channel: Channel,
+}
+
+#[derive(Debug, Deserialize, Getters)]
+#[serde(rename_all = "snake_case")]
+pub struct ChannelToneDetected {
     channel: Channel,
 }
 
