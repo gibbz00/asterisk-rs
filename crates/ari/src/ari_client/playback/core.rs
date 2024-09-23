dbbfb071bcf61cc14bc37990120dbedabcf9d72b
gibbz00 <gabrielhansson00@gmail.com>
2024-09-23 19:50:52 +0200

    feat(ari): add `AriClientplayback_stop`

--- crates/ari/src/ari_client/playback/core.rs
+++ crates/ari/src/ari_client/playback/core.rs
@@ -1,15 +1,17 @@
 use crate::*;
 
 impl AriClient {
+    const PLAYBACK_PATH: &str = "playbacks";
+
+    pub async fn playback_stop(&self, playback_id: &PlaybackId) -> AriClientResult<()> {
+        self.authorized_delete([Self::PLAYBACK_PATH], playback_id).await
+    }
+
     // pub async fn playback_get(&self, _playback_id: &PlaybackId) -> AriClientResult<Playback> {
     //     unimplemented!()
     // }
 
     // pub async fn playback_control(&self, _playback_id: &PlaybackId, _operation: Operation) -> AriClientResult<()> {
-    //     unimplemented!()
-    // }
-
-    // pub async fn playback_stop(&self, _playback_id: &PlaybackId) -> AriClientResult<()> {
     //     unimplemented!()
     // }
 }
