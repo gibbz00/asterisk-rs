5e32532e66a11807a33fadca4c8c37b64cf09841
gibbz00 <gabrielhansson00@gmail.com>
2024-09-23 20:43:34 +0200

    feat(ari): make `Bridge` fields public

--- crates/ari/src/ari_client/bridge/responses.rs
+++ crates/ari/src/ari_client/bridge/responses.rs
@@ -1,11 +1,10 @@
-use derive_getters::Getters;
 use serde::Deserialize;
 
 use crate::*;
 
-#[derive(Debug, Deserialize, Getters)]
+#[derive(Debug, Deserialize)]
 pub struct Bridge {
-    id: BridgeId,
+    pub id: BridgeId,
     // TODO: add remaining fields:
     // https://docs.asterisk.org/Asterisk_22_Documentation/API_Documentation/Asterisk_REST_Interface/Asterisk_REST_Data_Models/
 }
