1b45b7a8046121d3f50ec2dd0d49371f037ae8d4
gibbz00 <gabrielhansson00@gmail.com>
2024-09-23 20:51:10 +0200

    fix(ari): make bridge request parameter public

--- crates/ari/src/ari_client/bridge/request_params.rs
+++ crates/ari/src/ari_client/bridge/request_params.rs
@@ -7,19 +7,19 @@ use crate::*;
 pub struct BridgeAddChannelParams<'a> {
     /// Ids of channels to add to bridge, at least one is required
     #[serde(rename = "channel", serialize_with = "join_serialize")]
-    channels: &'a [&'a ChannelId],
+    pub channels: &'a [&'a ChannelId],
     /// Channel's role in the bridge
     // IMPROVEMENT: typeset as enum?
-    role: Option<&'a str>,
+    pub role: Option<&'a str>,
     /// Absorb DTMF coming from this channel, preventing it to pass through to the bridge
     #[serde(default, rename = "absorbDTMF")]
-    absorb_dtmf: bool,
+    pub absorb_dtmf: bool,
     /// Mute audio from this channel, preventing it to pass through to the bridge
     #[serde(default)]
-    mute: bool,
+    pub mute: bool,
     /// Do not present the identity of the newly connected channel to other bridge members
     #[serde(default)]
-    inhibit_connected_line_updates: bool,
+    pub inhibit_connected_line_updates: bool,
 }
 
 #[derive(Debug, Serialize)]
@@ -27,5 +27,5 @@ pub struct BridgeAddChannelParams<'a> {
 pub struct BridgeRemoveChannelParams<'a> {
     /// Ids of channels to add to bridge, at least one is required
     #[serde(rename = "channel", serialize_with = "join_serialize")]
-    channels: &'a [&'a ChannelId],
+    pub channels: &'a [&'a ChannelId],
 }
